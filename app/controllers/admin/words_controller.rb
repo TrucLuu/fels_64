368f7bdce405b2cf0a01f2c5b20014480e852369
truongnv0211 <nguyen.van.truong@framgia.com>
2015-06-29 08:45:55 +0700

    Admin word: index - show - create - update - delete

---
+++ app/controllers/admin/words_controller.rb
@@ -0,0 +1,57 @@
+class Admin::WordsController < ApplicationController
+  before_action :required_admin
+  before_action :set_word, except: [:index, :new, :create]
+
+  def index
+    @words = Word.paginate page: params[:page], per_page: Settings.words_per_page
+  end
+
+  def show
+  end
+
+  def new
+    @categories = Category.all
+    @word = Word.new
+    @word.answers.build
+  end
+
+  def create
+    @categories = Category.all
+    @word = Word.new word_params
+    if @word.save
+      flash[:success] = t :word_create_success
+      redirect_to admin_words_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @categories = Category.all
+  end
+
+  def update
+    if @word.update word_params
+      flash[:success] = t :word_update_success
+      redirect_to admin_words_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @word.destroy
+    flash[:success] = t :word_delete_success
+    redirect_to admin_words_path
+  end
+
+  private
+  def set_word
+    @word = Word.find params[:id]
+  end
+
+  def word_params
+    params.require(:word).permit :jp_word, :category_id,
+      answers_attributes: [:id, :answer_content, :correct, :_destroy]
+  end
+end
